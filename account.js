b32c631c6fe32f6223f003df09f76a4a79485554
Annoyshah <shahaamay646@gmail.com>
2022-06-29 20:07:41 +0530

    commit

---
+++ account.js
@@ -0,0 +1,4 @@
+let btn=document.querySelector("#io1")
+btn.onclick = function(){
+    sidebar.classList.toggle(active);
+}
---
+++ school.js
@@ -0,0 +1,26 @@
+
+var loader=document.getElementById("#preloader");
+window.addEventListener("load",function(){
+  preloader.style.display="none";
+})
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+
